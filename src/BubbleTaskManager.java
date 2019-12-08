33cefd4300f1e40c833f3f34b4502d46317c594f
Markus Dihlmann <markus.dihlmann@mathematik.uni-stuttgart.de>
2019-12-08 14:54:26 +0100

    initial commit

---
+++ src/BubbleTaskManager.java
@@ -0,0 +1,13 @@
+/*
+ * Main Class for the bubble task task manager
+ */
+
+public class BubbleTaskManager {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hallo Welt!");
+
+	}
+
+}
