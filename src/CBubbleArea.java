8bb1ff3c20d28a48bd49543a5e670721b0a99654
Markus <>
2020-01-03 02:58:23 +0100

    restructuring of bubble class

--- src/CBubbleArea.java
+++ src/CBubbleArea.java
@@ -7,6 +7,7 @@ public class CBubbleArea extends Canvas{
 	private static final int MAX_SET_SIZE = 500;
 	private static final int MIN_BUBBLE_RADIUS = 60;
 	private static final int MAX_BUBBLE_RADIUS = 120;
+	private static final int LIMIT_SMALL_RADIUS = 90;
 	
 	public static final Color BackgroundColor = new Color(250, 235, 215);
 	public static final Color urgentColor = new Color(178, 34, 34);
@@ -15,12 +16,127 @@ public class CBubbleArea extends Canvas{
 	public static Font smallFont = new Font("Avenir", Font.PLAIN, 10);
 	public static Font mediumFont = new Font("Avenir", Font.PLAIN, 12);
 	
-	class CBubble
+	public class CBubble
 	{
 		int x,y;
-		double radius;
-		Color fillColor;
-		int taskIndex; //taskIndex in taskList 
+		private int radius;
+		private Color fillColor;
+		private String text;
+		private CTask task;  
+		
+		CBubble(CTask task)
+		{
+			updateBubbleFromTask(task);
+			this.x = 0;
+			this.y = 0;
+		}
+		
+		private int computeRadius(double prio)
+		{
+			double prioNormal = (prio - CTask.MIN_PRIORITY)/(CTask.MAX_PRIORITY - CTask.MIN_PRIORITY);
+			double radiusDouble = prioNormal * (double)(MAX_BUBBLE_RADIUS - MIN_BUBBLE_RADIUS) + (double)(MIN_BUBBLE_RADIUS);
+			return (int) radiusDouble; 
+		}
+		
+		private Color computeColor(double prio)
+		{
+			Color retColor = bubbleColor;
+			double limit = (2.0/3.0) * (CTask.MAX_PRIORITY - CTask.MIN_PRIORITY) + CTask.MIN_PRIORITY;
+			if(prio > limit)
+				retColor = urgentColor;
+			
+			return retColor;
+		}
+		
+		public void drawBubble(Graphics g)
+		{
+			g.setColor(fillColor);
+			g.fillOval(x, y, radius, radius);
+			int lineIncrement = 11;
+			
+			g.setColor(Color.WHITE);
+			if(radius < LIMIT_SMALL_RADIUS)
+			{
+				g.setFont(smallFont);
+				lineIncrement = 11;
+			}
+			else
+			{
+				g.setFont(mediumFont);
+				lineIncrement = 14;
+			}
+			
+			String[] str = textUmbrechen(text, radius);
+			for(int i=0; i < str.length; i++)
+			{
+					g.drawString(str[i], x-radius+10, y-radius + i*lineIncrement);
+			}
+			
+		}
+		
+		private String[] textUmbrechen(String text, int radius)
+		{
+			String[] out = {" ", " ", " "};
+			
+			int charPerLine  = 7;
+			
+			if(radius > 70 && radius <= 90)
+				charPerLine = 11;
+			else
+			{
+				if(radius > 90 && radius <=110)
+					charPerLine = 14;
+				else
+				{
+					if(radius > 110)
+						charPerLine = 16;
+				}
+			}
+			
+			String str = text;
+			int indexSpace;
+			int charsUsed = 0;
+			
+			for(int i=0; i < out.length; i++)
+			{
+				out[i] = "";
+				indexSpace = str.indexOf(" ");
+				if(indexSpace > charPerLine || indexSpace == -1)
+				{
+					if(str.length()<=charPerLine)
+					{
+						out[i] = str;
+					}
+					else
+					{
+						out[i] = str.substring(0, charPerLine) + "...";
+						str = str.substring(indexSpace + 1);
+					}
+				}
+				else
+				{
+					charsUsed = indexSpace;
+					while(charsUsed <= charPerLine & indexSpace != -1)
+					{	
+						out[i] = out[i] + str.substring(0,indexSpace) + " ";
+						str = str.substring(indexSpace + 1);
+						indexSpace = str.indexOf(" ");
+						charsUsed += indexSpace; 
+					}
+				}
+			}
+			
+			return out;
+		}
+		
+		
+		public void updateBubbleFromTask(CTask task)
+		{
+			this.task = task;
+			this.radius = computeRadius(task.getPriority());
+			this.text = task.getDescription();
+			this.fillColor = computeColor(task.getPriority());
+		}
 	}
 	
 	CBubble[] bubbleSet = new CBubble[MAX_SET_SIZE];
@@ -37,29 +153,10 @@ public class CBubbleArea extends Canvas{
 	
 	public void paint(Graphics g)
 	{
-		g.setColor(bubbleColor);
-		g.fillOval(100, 100, 70, 70);
 		
-		g.setColor(Color.WHITE);
-		g.setFont(smallFont);
-		String[] str = textUmbrechenSmall("Aufgabe eins dringend");
-		for(int i=0; i < str.length; i++)
-		{
-				g.drawString(str[i], 110, 120 + i*11);
-		}
-		
-		g.setColor(urgentColor);
-		g.fillOval(200, 200, MAX_BUBBLE_RADIUS, MAX_BUBBLE_RADIUS);
-		
-		str = textUmbrechenBig("Dies ist eine groessere Aufgabe");
-		g.setColor(Color.white);
-		g.setFont(mediumFont);
-		for(int i=0; i < str.length; i++)
-		{
-				g.drawString(str[i], 210, 250 + i*14);
-		}
 	}
 	
+	/*
 	private String[] textUmbrechenSmall(String text)
 	{
 		String[] out = {" ", " ", " "};
@@ -104,48 +201,8 @@ public class CBubbleArea extends Canvas{
 		
 		return out;
 	}
+	*/
 	
-	private String[] textUmbrechenBig(String text)
-	{
-		String[] out = {" ", " ", " "};
-		
-		int charPerLine = 15;
-		
-		String str = text;
-		int indexSpace;
-		int charsUsed = 0;
-		
-		for(int i=0; i < out.length; i++)
-		{
-			out[i] = "";
-			indexSpace = str.indexOf(" ");
-			if(indexSpace > charPerLine || indexSpace == -1)
-			{
-				if(str.length()<=charPerLine)
-				{
-					out[i] = str;
-				}
-				else
-				{
-					out[i] = str.substring(0, charPerLine) + "...";
-					str = str.substring(indexSpace + 1);
-				}
-			}
-			else
-			{
-				charsUsed = indexSpace;
-				while(charsUsed <= charPerLine & indexSpace != -1)
-				{	
-					out[i] = out[i] + str.substring(0,indexSpace) + " ";
-					str = str.substring(indexSpace + 1);
-					indexSpace = str.indexOf(" ");
-					charsUsed += indexSpace; 
-				}
-			}
-		}
-		
-		return out;
-	}
 	
 	
 	
